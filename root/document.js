45507a629d7b54ade242ff357bc916f527a250b5
Bhagyesh <143402374+BhagyeshGohel@users.noreply.github.com>
2023-12-19 17:06:13 +0530

    Insta Hosting

---
+++ root/document.js
@@ -0,0 +1,23 @@
+let toggle = false;
+document.getElementById("btn").addEventListener("click", function(){
+    toggle =! toggle;
+    if(toggle){
+        document.getElementById("btn").innerHTML = "Following";
+        window.confirm("You want to UnFollow Harshil Patel");
+        document.getElementById("sidebar").style.display = "flex";
+        document.getElementById("box").style.height = "752px";
+
+        document.getElementById("line").style.display = "flex";
+        document.getElementById("follow").style.display = "flex"
+    }else{
+        document.getElementById("btn").innerHTML = "Un Following";
+        window.confirm("You want to Follow Harshil Patel");
+        document.getElementById("sidebar").style.display = "none";
+        document.getElementById("box").style.height = "300px";
+        document.getElementById("line").style.display = "none";
+        document.getElementById("follow").style.display = "none";
+    }
+})
+
+
+
